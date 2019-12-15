632b08414f88ee5adb05d8da580ae95b729efe6d
ngalinanes <nicolas.galinianes@gmail.com>
2019-12-14 23:08:39 -0300

    Se agrego el edit antecedentes familiares

--- app.py
+++ app.py
@@ -357,6 +357,41 @@ def edit_historia_personal(id):
     paciente = Paciente.query.filter_by(id=id).first()
     return render_template('historia_clinica/edit_historia_personal.html', historia_personal=historia_personal, paciente=paciente, title='Editar actividad física')
 
+@app.route('/edit_antecedentes_familiares/<int:id>', methods=['GET', 'POST'])
+def edit_antecedentes_familiares(id):
+    antecedentes_familiares = Antecedentes_familiares.query.filter_by(paciente_id=id).first()
+    if request.method == 'POST':
+        update_antecedentes_familiares = Antecedentes_familiares.query.filter_by(paciente_id=id).first()
+        if update_antecedentes_familiares == None:
+            new_antecedentes_familiares = Antecedentes_familiares(diabetes=request.form['diabetes'],
+                                                                cardiaca=request.form['cardiaca'],
+                                                                hipertension=request.form['hipertension'],
+                                                                sobrepeso=request.form['sobrepeso'],
+                                                                acv=request.form['acv'],
+                                                                cancer=request.form['cancer'],
+                                                                observaciones=request.form['observaciones'],
+                                                                otro_tca=request.form['otro_tca'], 
+                                                                paciente_id=id)
+            db.session.add(new_antecedentes_familiares)
+            db.session.commit()
+            flash('Has editado los antecedentes familiares del paciente con éxito.')
+            return redirect(url_for('ver_historia_clinica', id=id))
+        else:
+            update_antecedentes_familiares.diabetes = request.form['diabetes']
+            update_antecedentes_familiares.cardiaca = request.form['cardiaca']
+            update_antecedentes_familiares.hipertension = request.form['hipertension']
+            update_antecedentes_familiares.sobrepeso = request.form['sobrepeso']
+            update_antecedentes_familiares.acv = request.form['acv']
+            update_antecedentes_familiares.cancer = request.form['cancer']
+            update_antecedentes_familiares.observaciones = request.form['observaciones']
+            update_antecedentes_familiares.otro_tca = request.form['otro_tca']
+            update_antecedentes_familiares.paciente_id = id
+            db.session.commit()
+            flash('Has editado los antecedentes familiares del paciente con éxito.')
+            return redirect(url_for('ver_historia_clinica', id=id))
+    paciente = Paciente.query.filter_by(id=id).first()
+    return render_template('historia_clinica/edit_antecedentes_familiares.html', antecedentes_familiares=antecedentes_familiares, paciente=paciente, title='Editar antecedentes familiares')
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
  
